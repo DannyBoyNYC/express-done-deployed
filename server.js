b8813478583934da6559a9d8ea3ef9bd43d970c4
Daniel Deverell <daniel.deverell@gmail.com>
2021-11-27 17:52:29 -0600

    updated dburl

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const app = express();
 
 const dataBaseURL = process.env.DATABASE || "mongodb://localhost:27017";
 // const dataBaseURL = "mongodb://localhost:27017"; // use the local docker database
-// const dataBaseURL = "mongodb+srv://daniel:dd2345@recipescluster.anocj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"; // use the hosted database
+// const dataBaseURL = "mongodb+srv://daniel:dd2345@cluster0.bs2la.mongodb.net/recipes?retryWrites=true&w=majority"; // use the hosted database
 
 console.log("dataBaseURL::", dataBaseURL);
 
